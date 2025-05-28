ff491dd89eca4100ef507b30f2e2f9a1126902b8
Esteban Soler <esteban@informalthinkers.com>
2025-05-28 19:54:18 -0300

    create a linux distro WIP #17

---
+++ tools/image/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Path to pi-gen
+PIGEN_DIR="./pi-gen"
+
+# Ensure pi-gen is cloned
+if [ ! -d "$PIGEN_DIR" ]; then
+  echo "Cloning pi-gen..."
+  git clone https://github.com/RPi-Distro/pi-gen.git "$PIGEN_DIR"
+fi
+
+# Copy config into pi-gen
+cp ./tools/image/config "$PIGEN_DIR/config"
+
+# Copy custom stage
+cp -r ./tools/image/stage-gaucho "$PIGEN_DIR/"
+
+# Build image using Docker
+cd "$PIGEN_DIR"
+sudo ./build-docker.sh
