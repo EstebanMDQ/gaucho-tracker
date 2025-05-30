262269b946221feb1db1932aa6846d250de59784
Esteban Soler <esteban@informalthinkers.com>
2025-05-29 23:06:23 -0300

    image build script WIP #17

--- tools/image/build.sh
+++ tools/image/build.sh
@@ -10,12 +10,35 @@ if [ ! -d "$PIGEN_DIR" ]; then
   git clone https://github.com/RPi-Distro/pi-gen.git "$PIGEN_DIR"
 fi
 
+# Build the Rust application
+echo "Building Gaucho Tracker Rust application..."
+cargo build --release --manifest-path ./Cargo.toml
+
+# Copy the Rust binary to the custom stage
+echo "Copying Gaucho Tracker binary..."
+mkdir -p ./tools/image/stage-gaucho/01-gaucho-tracker/files/usr/local/bin
+cp ./target/release/gaucho-tracker ./tools/image/stage-gaucho/01-gaucho-tracker/files/usr/local/bin/
+
+# Ensure the required directory structure exists
+echo "Ensuring directory structure exists..."
+mkdir -p "$PIGEN_DIR/work/gaucho-os-lite/stage0/rootfs/etc/apt/"
+
 # Copy config into pi-gen
 cp ./tools/image/config "$PIGEN_DIR/config"
 
 # Copy custom stage
 cp -r ./tools/image/stage-gaucho "$PIGEN_DIR/"
 
+# Detect the operating system
+OS=$(uname -s)
+
+# Use sudo only if running on Linux
+if [ "$OS" = "Linux" ]; then
+  SUDO="sudo"
+else
+  SUDO=""
+fi
+
 # Build image using Docker
 cd "$PIGEN_DIR"
-./build-docker.sh
+$SUDO ./build-docker.sh
